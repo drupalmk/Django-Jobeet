0bc04fc6a6e63fc16db8577ecd0d93fbf6ba881b
drupalmk <leisikkeram@gmail.com>
2012-03-18 22:12:58 +0100

    fix varchars length in models

--- jobs/models.py
+++ jobs/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Categories(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(unique=True, max_length=765)
-    slug = models.CharField(max_length=765)
+    name = models.CharField(unique=True, max_length=255)
+    slug = models.CharField(max_length=255)
     class Meta:
         db_table = u'categories'
 
@@ -11,17 +11,17 @@ class Jobs(models.Model):
     id = models.IntegerField(primary_key=True)
     category = models.ForeignKey(Categories, null=True, blank=True)
     user_id = models.IntegerField(null=True, blank=True)
-    job_type = models.CharField(max_length=765)
-    company = models.CharField(max_length=765)
-    logo = models.CharField(max_length=765, blank=True)
-    url = models.CharField(max_length=765, blank=True)
-    position = models.CharField(max_length=765, blank=True)
-    location = models.CharField(max_length=765)
-    description = models.CharField(max_length=12000)
-    how_to_apply = models.CharField(max_length=12000)
+    job_type = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    logo = models.CharField(max_length=255, blank=True)
+    url = models.CharField(max_length=255, blank=True)
+    position = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=4000)
+    how_to_apply = models.CharField(max_length=4000)
     is_public = models.IntegerField()
     is_activated = models.IntegerField()
-    email = models.CharField(max_length=765)
+    email = models.CharField(max_length=255)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(null=True, blank=True)
     expires_at = models.DateTimeField()
