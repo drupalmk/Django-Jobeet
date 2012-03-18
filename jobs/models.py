751213a7809ff4533b374c4f18fd9f55b6005fc1
drupalmk <leisikkeram@gmail.com>
2012-03-18 22:07:43 +0100

    Setup jobs and categories models from db by introspection with wrong length of varchar column:

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(unique=True, max_length=765)
+    slug = models.CharField(max_length=765)
+    class Meta:
+        db_table = u'categories'
+
+class Jobs(models.Model):
+    id = models.IntegerField(primary_key=True)
+    category = models.ForeignKey(Categories, null=True, blank=True)
+    user_id = models.IntegerField(null=True, blank=True)
+    job_type = models.CharField(max_length=765)
+    company = models.CharField(max_length=765)
+    logo = models.CharField(max_length=765, blank=True)
+    url = models.CharField(max_length=765, blank=True)
+    position = models.CharField(max_length=765, blank=True)
+    location = models.CharField(max_length=765)
+    description = models.CharField(max_length=12000)
+    how_to_apply = models.CharField(max_length=12000)
+    is_public = models.IntegerField()
+    is_activated = models.IntegerField()
+    email = models.CharField(max_length=765)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    expires_at = models.DateTimeField()
+    class Meta:
+        db_table = u'jobs'
