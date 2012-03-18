697bf376ce1bb63081f3d0617bfe659b20e1dda7
drupalmk <leisikkeram@gmail.com>
2012-03-18 22:15:24 +0100

    fix models fields blank status

--- jobs/models.py
+++ jobs/models.py
@@ -9,13 +9,13 @@ class Categories(models.Model):
 
 class Jobs(models.Model):
     id = models.IntegerField(primary_key=True)
-    category = models.ForeignKey(Categories, null=True, blank=True)
-    user_id = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(Categories, null=True)
+    user_id = models.IntegerField(null=True)
     job_type = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
-    logo = models.CharField(max_length=255, blank=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
     url = models.CharField(max_length=255, blank=True)
-    position = models.CharField(max_length=255, blank=True)
+    position = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     description = models.CharField(max_length=4000)
     how_to_apply = models.CharField(max_length=4000)
